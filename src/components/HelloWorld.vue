4bd99da474616f3b67d5ece3b3483b2005375391
Telmo "Trooper <telmo.trooper@gmail.com>
2018-05-09 21:03:28 -0300

    Base code

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <ion-app>
+    <ion-page class="show-page">
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Ionic 4 + Vue Application </ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content class="content" padding>
+          <ion-list>
+            <ion-item v-for="item of items" v-bind:key="item.id">
+              <ion-label full>{{ item.name }}</ion-label>
+            </ion-item>
+          </ion-list>
+      </ion-content>
+      <ion-footer>
+      <ion-button @click="goToPage2" full>Go to Page 2</ion-button>
+  </ion-footer>
+    </ion-page>
+  </ion-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      items: [{id: 0, name: 'Ahmed'}, {id: 1, name: 'Naima'}]
+    }
+  },
+  methods: {
+    goToPage2 () {
+      this.$router.push('page2')
+    }
+  }
+}
+</script>
---
+++ src/components/Page2.vue
@@ -0,0 +1,32 @@
+<template>
+  <ion-app>
+    <ion-page class="show-page">
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Ionic 4 + Vue Application </ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content class="content" padding>
+        This is page 2
+      </ion-content>
+      <ion-footer>
+        <ion-button @click="goHome" full>Go home</ion-button>
+      </ion-footer>
+    </ion-page>
+  </ion-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    goHome () {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
